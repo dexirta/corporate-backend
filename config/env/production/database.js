b8b11202a06a9b3f9176961f67fdfcfbce5ef1ff
dezso <dexirta@hotmail.com>
2023-03-09 12:59:20 +0100

    fix(app): #0 removed mysql connection for testing

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,23 +1,13 @@
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'mysql',
-    connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL', false) && {
-        key: env('DATABASE_SSL_KEY', undefined),
-        cert: env('DATABASE_SSL_CERT', undefined),
-        ca: env('DATABASE_SSL_CA', undefined),
-        capath: env('DATABASE_SSL_CAPATH', undefined),
-        cipher: env('DATABASE_SSL_CIPHER', undefined),
-        rejectUnauthorized: env.bool(
-          'DATABASE_SSL_REJECT_UNAUTHORIZED',
-          true
-        ),
-      },
-    },
-  },
+  // connection: {
+  //   client: 'mysql',
+  //   connection: {
+  //     host: env('DATABASE_HOST'),
+  //     port: env.int('DATABASE_PORT'),
+  //     database: env('DATABASE_NAME'),
+  //     user: env('DATABASE_USERNAME'),
+  //     password: env('DATABASE_PASSWORD'),
+  //     ssl: env.bool('DATABASE_SSL', false)
+  //   },
+  // },
 });
